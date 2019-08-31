90db8272d6b6f27a1513b41a297268129f4e01e7
VISHAL <VISHAL@HP>
2019-08-31 15:47:20 +0530

    SQLLite Database Configuration Changes

--- SpringBootJFX/src/main/java/com/billing/utils/DBUtils.java
+++ SpringBootJFX/src/main/java/com/billing/utils/DBUtils.java
@@ -9,6 +9,8 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
