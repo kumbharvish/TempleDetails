e9e3ca7a39ea75c67d96cca5228433d765d24f18
VISHAL <VISHAL@HP>
2019-09-26 22:47:18 +0530

    Excluded unwanted dependencies

--- SpringBootJFX/src/main/java/com/billing/controller/PrintBarcodeController.java
+++ SpringBootJFX/src/main/java/com/billing/controller/PrintBarcodeController.java
@@ -57,10 +57,10 @@ public class PrintBarcodeController extends AppContext implements TabContent {
 
 	@Autowired
 	AppUtils appUtils;
-	
+
 	@Autowired
 	JasperUtils jasperUtils;
-	
+
 	@Autowired
 	JasperService jasperService;
 
@@ -71,49 +71,49 @@ public class PrintBarcodeController extends AppContext implements TabContent {
 	private TabPane tabPane = null;
 
 	int productCode = 0;
-	
+
 	private HashMap<String, Product> productMap;
 
 	private SortedSet<String> entries;
 
-    @FXML
-    private AutoCompleteTextField txtName;
+	@FXML
+	private AutoCompleteTextField txtName;
 
-    @FXML
-    private TextField txtQuantity;
+	@FXML
+	private TextField txtQuantity;
 
-    @FXML
-    private TextField txtCategory;
+	@FXML
+	private TextField txtCategory;
 
-    @FXML
-    private TextField txtBarcode;
+	@FXML
+	private TextField txtBarcode;
 
-    @FXML
-    private Button btnGenerateBarcode;
+	@FXML
+	private Button btnGenerateBarcode;
 
-    @FXML
-    private RadioButton rb_65Stickers;
+	@FXML
+	private RadioButton rb_65Stickers;
 
-    @FXML
-    private RadioButton rb_40Stickers;
+	@FXML
+	private RadioButton rb_40Stickers;
 
-    @FXML
-    private RadioButton rb_24Stickers;
+	@FXML
+	private RadioButton rb_24Stickers;
 
-    @FXML
-    private TextField txtStartPosition;
+	@FXML
+	private TextField txtStartPosition;
 
-    @FXML
-    private TextField txtNoOfBarcodes;
-    
-    @FXML
-    private TextField txtSellPrice;
+	@FXML
+	private TextField txtNoOfBarcodes;
 
-    @FXML
-    private Button btnClose;
+	@FXML
+	private TextField txtSellPrice;
 
-    @FXML
-    private ImageView imageView;
+	@FXML
+	private Button btnClose;
+
+	@FXML
+	private ImageView imageView;
 
 	@Override
 	public void initialize() {
@@ -122,99 +122,95 @@ public class PrintBarcodeController extends AppContext implements TabContent {
 		rb_24Stickers.setToggleGroup(radioButtonGroup);
 		rb_40Stickers.setToggleGroup(radioButtonGroup);
 		rb_65Stickers.setToggleGroup(radioButtonGroup);
-		
+
 		txtCategory.textProperty().addListener((observable, oldValue, newValue) -> {
 			btnGenerateBarcode.setDisable(newValue.isEmpty());
 		});
 		btnGenerateBarcode.setDisable(true);
-		
+
 		getProductsName();
 		txtName.createTextField(entries, () -> setProductDetails());
 		txtStartPosition.setText("1");
 		txtNoOfBarcodes.setText("65");
-		
+
 		rb_65Stickers.setOnAction(e -> populateImageView());
 		rb_24Stickers.setOnAction(e -> populateImageView());
 		rb_40Stickers.setOnAction(e -> populateImageView());
-		
+
 		rb_65Stickers.setSelected(true);
-		try {
-			imageView.setImage(new Image(new FileInputStream("src\\main\\resources\\images\\65_Labels.png")));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			logger.error(e.getMessage());;
-		}
+		imageView.setImage(new Image(this.getClass().getResource("/images/65_Labels.png").toString()));
 	}
 
 	private void populateImageView() {
-		String imageName="65_Labels.png";
-		if(rb_65Stickers.isSelected()) {
-			imageName="65_Labels.png";
+		String imageName = "65_Labels.png";
+		if (rb_65Stickers.isSelected()) {
+			imageName = "65_Labels.png";
 			txtStartPosition.setText("1");
 			txtNoOfBarcodes.setText("65");
-		}else if(rb_40Stickers.isSelected()) {
-			imageName="40_Labels.png";
+		} else if (rb_40Stickers.isSelected()) {
+			imageName = "40_Labels.png";
 			txtStartPosition.setText("1");
 			txtNoOfBarcodes.setText("40");
-		}else {
-			imageName="24_Labels.png";
+		} else {
+			imageName = "24_Labels.png";
 			txtStartPosition.setText("1");
 			txtNoOfBarcodes.setText("24");
 		}
-		try {
-			imageView.setImage(new Image(new FileInputStream("src\\main\\resources\\images\\"+imageName)));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			logger.error(e.getMessage());;
-		}
+
+		imageView.setImage(new Image(this.getClass().getResource("/images/" + imageName).toString()));
 	}
 
 	@FXML
 	void onGenerateBarcodeCommand(ActionEvent event) {
-		if (txtName.getText().equals("") && !txtBarcode.getText().equals("")) {
+		if (txtName.getText().equals("")) {
 			alertHelper.showErrorNotification("Please select product");
+		} else if (txtBarcode.getText().equals("")) {
+			alertHelper.showErrorNotification("Please generate the barcode for product");
 		} else {
+
 			String fileName = txtName.getText();
 			String JrxmlName = null;
 			int startPosition = 1;
 			int noOfLabels = 0;
-				try {
-					Barcode barcode = new  Barcode();
-					barcode.setBarcode(txtBarcode.getText().trim());
-					barcode.setPrice(Double.valueOf(txtSellPrice.getText()));
-					barcode.setProductName(txtName.getText());
-					
-					if(rb_65Stickers.isSelected()) {
-						JrxmlName = AppConstants.BARCODE_65_JASPER;
-						noOfLabels = 65;
-					}else if (rb_24Stickers.isSelected()) {
-						JrxmlName = AppConstants.BARCODE_24_JASPER;
-						noOfLabels = 24;
-					}else if(rb_40Stickers.isSelected()) {
-						JrxmlName = AppConstants.BARCODE_40_JASPER;
-						noOfLabels = 40;
-					}
-					if(!txtStartPosition.getText().equals("")) {
-						startPosition = Integer.valueOf(txtStartPosition.getText());
-					}else {
-						alertHelper.showErrorNotification("Please enter start position");
-						return;
-					}
-					if(!txtNoOfBarcodes.getText().equals("")) {
-						noOfLabels = Integer.valueOf(txtNoOfBarcodes.getText());
-					}else {
-						alertHelper.showErrorNotification("Please enter no of barcodes");
-						return;
-					}
-					
-					boolean isSuccess = jasperUtils.createPDFForBarcode(jasperService.createDataForBarcode(barcode,noOfLabels,startPosition), JrxmlName, fileName);
-					if(!isSuccess) {
-						alertHelper.showErrorNotification("Barcode length should be 12 digits! Please correct barcode number");
-					}
-				} catch (Exception e1) {
-					e1.printStackTrace();
-					logger.error("Print Barcode Sheet Exception : ",e1);
-				} 
+			try {
+				Barcode barcode = new Barcode();
+				barcode.setBarcode(txtBarcode.getText().trim());
+				barcode.setPrice(Double.valueOf(txtSellPrice.getText()));
+				barcode.setProductName(txtName.getText());
+
+				if (rb_65Stickers.isSelected()) {
+					JrxmlName = AppConstants.BARCODE_65_JASPER;
+					noOfLabels = 65;
+				} else if (rb_24Stickers.isSelected()) {
+					JrxmlName = AppConstants.BARCODE_24_JASPER;
+					noOfLabels = 24;
+				} else if (rb_40Stickers.isSelected()) {
+					JrxmlName = AppConstants.BARCODE_40_JASPER;
+					noOfLabels = 40;
+				}
+				if (!txtStartPosition.getText().equals("")) {
+					startPosition = Integer.valueOf(txtStartPosition.getText());
+				} else {
+					alertHelper.showErrorNotification("Please enter start position");
+					return;
+				}
+				if (!txtNoOfBarcodes.getText().equals("")) {
+					noOfLabels = Integer.valueOf(txtNoOfBarcodes.getText());
+				} else {
+					alertHelper.showErrorNotification("Please enter no of barcodes");
+					return;
+				}
+
+				boolean isSuccess = jasperUtils.createPDFForBarcode(
+						jasperService.createDataForBarcode(barcode, noOfLabels, startPosition), JrxmlName, fileName);
+				if (!isSuccess) {
+					alertHelper
+							.showErrorNotification("Barcode length should be 12 digits! Please correct barcode number");
+				}
+			} catch (Exception e1) {
+				e1.printStackTrace();
+				logger.error("Print Barcode Sheet Exception : ", e1);
+			}
 		}
 	}
 
@@ -292,14 +288,18 @@ public class PrintBarcodeController extends AppContext implements TabContent {
 		txtNoOfBarcodes.setText("65");
 		productCode = 0;
 	}
-	
+
 	private void setProductDetails() {
 		Product p = productMap.get(txtName.getText());
-		if(p!=null) {
+		if (p != null) {
 			txtCategory.setText(p.getProductCategory());
 			txtQuantity.setText(appUtils.getDecimalFormat(p.getQuantity()));
 			txtSellPrice.setText(appUtils.getDecimalFormat(p.getSellPrice()));
-			txtBarcode.setText(String.valueOf(p.getProductBarCode()));
+			if (p.getProductBarCode() == 0) {
+				txtBarcode.setText("");
+			} else {
+				txtBarcode.setText(String.valueOf(p.getProductBarCode()));
+			}
 		}
 	}
 
